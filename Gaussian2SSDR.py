8dc743125e9ba0687cc796367b00dab36744cf6d
CookMaker <1015005313@qq.com>
2024-09-20 14:00:25 +0800

    update : correct the visualizer

--- Gaussian2SSDR.py
+++ Gaussian2SSDR.py
@@ -1,1 +1,297 @@
-# 读取4dgs的结果作为输入 ，使用ssdr解出B和W，并赋给mesh
+#  created by Isabella Liu (lal005@ucsd.edu) at 2024/05/29 18:21.
+#
+#  Rendering the trained model on the test dataset
+
+
+import os, sys
+import json
+import datetime
+import os.path as osp
+import torch
+import uuid
+import datetime
+from tqdm import tqdm
+import random
+from argparse import ArgumentParser, Namespace
+import numpy as np
+import imageio
+import cv2
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from mayavi import mlab
+import trimesh
+
+from own_ssdr import SSDR
+from scene import Scene
+from scene import GaussianModelDPSRDynamicAnchor as gaussian_model
+from scene import DeformModelNormal as deform_model
+from scene import DeformModelNormalSep as deform_model_sep
+from scene import AppearanceModel as appearance_model
+from utils.renderer import mesh_renderer, mesh_shape_renderer
+from utils.general_utils import safe_state
+from utils.system_utils import load_config_from_file, merge_config
+from arguments import ModelParams, PipelineParams, OptimizationParams
+
+import nvdiffrast.torch as dr
+
+try:
+    from torch.utils.tensorboard import SummaryWriter
+
+    TENSORBOARD_FOUND = True
+except ImportError:
+    TENSORBOARD_FOUND = False
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def save_pointcloud(pc,name="point_cloud.ply"):
+    cloud = trimesh.points.PointCloud(pc)
+    cloud.export(name)
+
+
+
+@torch.no_grad()
+def rendering_trajectory(dataset, opt, pipe, checkpoint, fps=24):
+    args.model_path = dataset.model_path
+
+    # Load models
+    ## Gaussian model
+    gaussians = gaussian_model(
+        dataset.sh_degree,
+        grid_res=dataset.grid_res,
+        density_thres=opt.init_density_threshold,
+        dpsr_sig=opt.dpsr_sig,
+    )
+    glctx = dr.RasterizeGLContext()
+    scene = Scene(dataset, gaussians, shuffle=False)
+    ## Deform forward model
+    deform = deform_model(
+        is_blender=dataset.is_blender, is_6dof=dataset.is_6dof, model_name="deform"
+    )
+    deform_normal = deform_model_sep(
+        is_blender=dataset.is_blender,
+        is_6dof=dataset.is_6dof,
+        model_name="deform_normal",
+    )
+    ## Deform backward model
+    deform_back = deform_model(
+        is_blender=dataset.is_blender, is_6dof=dataset.is_6dof, model_name="deform_back"
+    )
+    deform_back_normal = deform_model_sep(
+        is_blender=dataset.is_blender,
+        is_6dof=dataset.is_6dof,
+        model_name="deform_back_normal",
+    )
+    ## Appearance model
+    appearance = appearance_model(is_blender=dataset.is_blender)
+    ## Load checkpoint
+    if checkpoint:
+        gaussians.load_ply(checkpoint, iteration=-1)
+        deform.load_weights(checkpoint, iteration=-1)
+        deform_normal.load_weights(checkpoint, iteration=-1)
+        deform_back.load_weights(checkpoint, iteration=-1)
+        deform_back_normal.load_weights(checkpoint, iteration=-1)
+        appearance.load_weights(checkpoint, iteration=-1)
+
+    # Compose camera trajectory
+    viewpoint_cam_stack = scene.getTrainCameras().copy()
+    # Create folders
+    image_folder = osp.join(dataset.model_path, "images")
+    os.makedirs(image_folder, exist_ok=True)
+    final_images = []
+    gaussian_point_cloud = []
+
+    for idx, viewpoint_cam in tqdm(enumerate(viewpoint_cam_stack)):
+
+        fid = viewpoint_cam.fid
+        N = gaussians.get_xyz.shape[0]
+        time_input = fid.unsqueeze(0).expand(N, -1)
+
+        # Query the gaussians
+        d_xyz, d_rotation, d_scaling, _ = deform.step(
+            gaussians.get_xyz.detach(), time_input
+        )
+        gaussian_point_cloud.append(gaussians.get_xyz.detach().cpu() + d_xyz.detach().cpu())
+    
+    # 拿到gaussian点云序列之后，送进SSDR
+    
+    # read skeleton from Converage Axis
+    Init_Bone = trimesh.load("/home/wjx/research/code/Coverage_Axis/output/mesh_selected_inner_points.obj").vertices
+    # print(Init_Bone.shape)
+    poses = np.array(gaussian_point_cloud)
+    rest_pose = poses[0]
+    # save_pointcloud(rest_pose,"outputs/ssdr/pc/pc.ply")
+
+
+
+    W, bone_transforms, rest_bones_t = SSDR(poses, rest_pose, num_bones=Init_Bone.shape[0],initial_Bone=Init_Bone)
+    np.save("outputs/ssdr/ssdr_result.npy",{"W":W, "Bone_Trans":bone_transforms, "rest_bone":rest_bones_t,"rest_pose":rest_pose})
+    ssdr_visualization(rest_pose,rest_bones_t,W,bone_transforms)
+
+    # 对B和W进行可视化
+def ssdr_visualization(rest_pose=None,rest_bone=None,W=None,bone_trans=None,eval=False):
+    if eval:
+        results = np.load("./ssdr_result.npy",allow_pickle=True).item()
+        rest_pose = results["rest_pose"]
+        W = results["W"]
+        rest_bone = results["rest_bone"]
+        bone_trans = results["Bone_Trans"]
+        print("W shape:", W.shape)
+        print("Bone Transforms shape:", bone_trans.shape)
+        print("Rest Bones shape:", rest_bone.shape)
+        print("Rest Pose shape:", rest_pose.shape)
+
+    # # 可视化点云
+    # mlab.figure(bgcolor=(1, 1, 1))
+    # mlab.points3d(rest_pose[:, 0], rest_pose[:, 1], rest_pose[:, 2], scale_factor=0.01)
+    # mlab.title("Point Cloud")
+    # mlab.show()
+
+    # # 假设bone_transforms是形状为[K, 4, 4]的骨骼变换矩阵
+    # # 假设rest_bones_t是rest pose下骨骼的位置
+
+    # K = rest_bone.shape[0]
+    # mlab.figure(bgcolor=(1, 1, 1))
+
+    # for i in range(K):
+    #     # 获取骨骼在rest pose下的位置
+    #     bone_pos = rest_bone[i]
+        
+    #     # 绘制骨骼的关节
+    #     mlab.points3d(bone_pos[0], bone_pos[1], bone_pos[2], scale_factor=0.05, color=(1, 0, 0))
+        
+    #     # 如果骨骼之间有连接，绘制骨骼
+    #     for j in range(i+1, K):
+    #         parent_pos = rest_bone[j]
+    #         mlab.plot3d([bone_pos[0], parent_pos[0]], [bone_pos[1], parent_pos[1]], [bone_pos[2], parent_pos[2]], color=(0, 0, 1))
+
+    # mlab.title("Skeleton")
+    # mlab.show()
+    for i in range(bone_trans.shape[0]):
+        bone_index = i  # 选择一个骨骼的索引
+        weights = W[:, bone_index]  # 获取对应骨骼的weights
+
+        mlab.figure(bgcolor=(1, 1, 1))
+        points = rest_pose
+        # 通过颜色显示skin weights
+        mlab.points3d(points[:, 0], points[:, 1], points[:, 2], weights, scale_factor=0.01, colormap='viridis')
+        mlab.title(f"Skinning Weights for Bone {bone_index}")
+        mlab.show()
+    # 计算变换后的点云
+    transformed_points = apply_bone_transforms(rest_pose, W, bone_trans)
+
+    mlab.figure(bgcolor=(1, 1, 1), size=(800, 800))
+    mlab.view(azimuth=45, elevation=80, distance=10)
+
+    # 迭代每一帧点云，保存为图片
+    for i, frame_points in enumerate(transformed_points):
+        # 清除之前的点云
+        mlab.clf()
+
+        # 绘制当前帧的点云
+        mlab.points3d(frame_points[:, 0], frame_points[:, 1], frame_points[:, 2],
+                    scale_factor=0.01, color=(0, 0, 1))
+        
+        # 保存图片
+        mlab.savefig(f'outputs/ssdr/frames/frame_{i:03d}.png')
+
+    # 关闭 mayavi 图形窗口
+    mlab.close()
+
+
+
+
+
+# 使用B和W驱动gaussian
+# 将骨骼变换应用到点云
+def apply_bone_transforms(rest_pose, W, bone_trans):
+    num_frames = bone_trans.shape[1]
+    transformed_points = []
+
+    for t in range(num_frames):
+        transformed_frame = np.zeros_like(rest_pose)
+        for i in range(W.shape[1]):
+            transform = bone_trans[i, t]
+            rotation = transform[:3, :3]
+            translation = transform[:3, 2]
+            transformed_frame += W[:, i:i+1] *( (rotation @ rest_pose.T).T + translation)
+        transformed_points.append(transformed_frame)
+
+    return transformed_points
+
+
+
+if __name__ == "__main__":
+    # Set up command line argument parser
+    parser = ArgumentParser(description="Rendering script parameters")
+    lp = ModelParams(parser)
+    op = OptimizationParams(parser)
+    pp = PipelineParams(parser)
+    parser.add_argument("--detect_anomaly", action="store_true", default=False)
+    parser.add_argument("--quiet", action="store_true")
+    parser.add_argument("--start_checkpoint", type=str, default=None)
+    parser.add_argument("--config", type=str, default=None)
+    parser.add_argument("--camera_radius", type=float, default=4.0)
+    parser.add_argument("--camera_lookat", type=float, nargs="+", default=[0, 0, 0])
+    parser.add_argument("--camera_elevation", type=float, default=1.0)
+    parser.add_argument("--fps", type=int, default=10)
+    parser.add_argument("--total_frames", type=int, default=100)
+    parser.add_argument("--eva", type=bool, default=False)
+
+    
+    # Fix random seed
+    torch.manual_seed(0)
+    np.random.seed(0)
+    random.seed(0)
+
+    args = parser.parse_args(sys.argv[1:])
+    if not args.eva:
+        # Load config file
+        if args.config:
+            config_data = load_config_from_file(args.config)
+            combined_args = merge_config(config_data, args)
+            args = Namespace(**combined_args)
+
+        lp = lp.extract(args)
+        op = op.extract(args)
+        pp = pp.extract(args)
+
+        # Updating save path
+        unique_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        data_name = osp.basename(lp.source_path)
+        folder_name = f"rendering-traj-{data_name}-{unique_str}"
+        if not lp.model_path:
+            if os.getenv("OAR_JOB_ID"):
+                unique_str = os.getenv("OAR_JOB_ID")
+            else:
+                unique_str = str(uuid.uuid4())
+            lp.model_path = os.path.join("./output/", unique_str[0:10])
+        lp.model_path = osp.join(lp.model_path, folder_name)
+        # Set up output folder
+        print("Output folder: {}".format(lp.model_path))
+        os.makedirs(lp.model_path, exist_ok=True)
+
+        # Initialize system state (RNG)
+        safe_state(args.quiet)
+
+        # Save all parameters into file
+        combined_args = vars(Namespace(**vars(lp), **vars(op), **vars(pp)))
+        # Convert namespace to JSON string
+        args_json = json.dumps(combined_args, indent=4)
+        # Write JSON string to a text file
+        with open(osp.join(lp.model_path, "cfg_args.txt"), "w") as output_file:
+            output_file.write(args_json)
+
+        torch.autograd.set_detect_anomaly(args.detect_anomaly)
+        rendering_trajectory(lp, op, pp, args.start_checkpoint, args.fps)
+
+        # All done
+        print("\nRendering complete.")
+    else:
+        ssdr_visualization(eval=args.eva)
+
+
+
+
+
+
--- pointcloud_viewer.py
+++ pointcloud_viewer.py
@@ -3,6 +3,68 @@ import os
 import numpy as np
 import trimesh
 from mayavi import mlab
+import subprocess
+
+
+
+def normalize_points(points):
+    # 计算最小值和最大值
+    min_vals = np.min(points, axis=0)
+    max_vals = np.max(points, axis=0)
+
+    # 归一化操作：将所有点的值缩放到 [0, 1] 范围
+    normalized_points = (points - min_vals) / (max_vals - min_vals) - 0.5
+    return normalized_points
+
+
+def check_path(path):
+    # 将相对路径转换为绝对路径
+    absolute_path = os.path.abspath(path)
+    
+    # 判断路径是否存在
+    if not os.path.exists(absolute_path):
+        # 如果路径不存在，创建路径
+        os.makedirs(absolute_path)
+        print(f"路径不存在，已创建路径: {absolute_path}")
+    else:
+        print(f"路径已存在: {absolute_path}")
+
+
+import os
+import subprocess
+
+def images_to_gif(path, fps, output_name="output.gif"):
+    # 检查输入路径是否存在
+    if not os.path.exists(path):
+        raise ValueError(f"路径 {path} 不存在")
+    
+    # 使用 ffmpeg 进行图片合成 gif
+    # 假设图片的命名为 img_001.png, img_002.png, img_003.png 等等
+    # 根据你的图片命名格式调整 pattern
+    pattern = os.path.join(path, "frame_%d.png")
+    
+    # 构建 ffmpeg 命令
+    cmd = [
+        "ffmpeg",
+        "-framerate", str(fps),         # 设置帧率
+        "-i", pattern,                  # 输入图片的路径模式
+        "-vf", "scale=800:-1",          # 可选：调整gif大小，800px宽度，高度根据宽度自动调整
+        "-loop", "0",                   # 设置 gif 循环播放，0 表示无限循环
+        f"outputs/ssdr/{output_name}"                     # 输出文件名
+    ]
+    
+    # 调用 subprocess 来执行命令
+    try:
+        subprocess.run(cmd, check=True)
+        print(f"GIF 成功生成，保存路径为: {output_name}")
+    except subprocess.CalledProcessError as e:
+        print(f"ffmpeg 命令执行失败: {e}")
+
+# 示例调用
+# images_to_gif("outputs/ssdr/images", fps=10, output_name="animation.gif")
+
+
+
 
 def visualize_and_save_point_cloud(input_path, output_path, camera_params=None, image_size=(800, 600)):
     # 检查输入路径是文件还是文件夹
@@ -93,6 +155,7 @@ def visualize_multiple_point_clouds(input_paths, output_path, colors=None, opaci
 
     print(f"Rendered image saved to: {output_path}")
 
+
 def reconstruct(rest_pose, bone_transforms, rest_bones_t, W):
     """
     Computes the skinned vertex positions on some poses given bone transforms and weights.
@@ -104,23 +167,55 @@ def reconstruct(rest_pose, bone_transforms, rest_bones_t, W):
              W               |num_verts| x |num_bones| matrix: bone-vertex weight map. Rows sum to 1, sparse.
 
     return: |num_poses| x |num_verts| x 3 Vertex positions for all poses: sum{bones} (w * (R @ p + T)) 
+            |num_bones| x |num_poses| x 3 Max weighted vertex positions
     """
     # Points in rest pose without rest bone translations
-    p_corrected = rest_pose[np.newaxis, :, :] - rest_bones_t[:, np.newaxis, :] # |num_bones| x |num_verts| x 3
-    constructions = np.einsum('bijk,blk->bilj', bone_transforms[:, :, :3, :], p_corrected) # |num_bones| x |num_poses| x |num_verts| x 3
-    constructions += bone_transforms[:, :, np.newaxis, 3, :] # |num_bones| x |num_poses| x |num_verts| x 3
-    constructions *= (W.T)[:, np.newaxis, :, np.newaxis] # |num_bones| x |num_poses| x |num_verts| x 3
-    return np.sum(constructions, axis=0)
+    p_corrected = rest_pose[np.newaxis, :, :] - rest_bones_t[:, np.newaxis, :]  # |num_bones| x |num_verts| x 3
+
+    # Compute the transformed vertex positions
+    constructions = np.einsum('bijk,blk->bilj', bone_transforms[:, :, :3, :],
+                              p_corrected)  # |num_bones| x |num_poses| x |num_verts| x 3
+    constructions += bone_transforms[:, :, np.newaxis, 3, :]  # |num_bones| x |num_poses| x |num_verts| x 3
+    constructions *= (W.T)[:, np.newaxis, :, np.newaxis]  # |num_bones| x |num_poses| x |num_verts| x 3
+    vertex_positions = np.sum(constructions, axis=0)  # |num_poses| x |num_verts| x 3
+
+    # 从W数组中提取出每个骨骼权重最大的点
+    # 找到每个骨骼在每个姿势下最大权重的顶点索引
+    max_weight_indices = np.argmax(W, axis=0)  # |num_verts| x |num_bones|
+    print("Max Weights:",W[max_weight_indices[0],0],W[max_weight_indices[1],1],W[max_weight_indices[2],2],W[max_weight_indices[3],3],W[max_weight_indices[4],4],W[max_weight_indices[5],5],W[max_weight_indices[6],6],W[max_weight_indices[7],7],W[max_weight_indices[8],8],W[max_weight_indices[9],9])
+
+    # 使用这些索引从vertex_positions中提取对应的点
+    max_weight_points = np.zeros((bone_transforms.shape[1], bone_transforms.shape[0], 3))
+    for bone in range(W.shape[1]):
+        for pose in range(vertex_positions.shape[0]):
+            idx = max_weight_indices[bone]
+            max_weight_points[pose, bone] = vertex_positions[pose, idx]
+
+    return vertex_positions
 
 
 def get_transformed_bones(bone_transforms, rest_bones_t):
-    # 提取旋转部分
-    rotation_matrices = bone_transforms[:, :, :3, :]
+    # # 提取旋转部分
+    # rotation_matrices = bone_transforms[:, :, :3, :]
     
-    # 计算变换后的骨骼位置
-    transformed_bones = np.einsum('bijk,bk->bij', rotation_matrices, rest_bones_t) + bone_transforms[:, :, 3, :]
+    # # 计算变换后的骨骼位置
+    # # transformed_bones = np.einsum('bijk,bk->bik', rotation_matrices, rest_bones_t) + bone_transforms[:, :, 3, :] # Bones, Frames, 3
+    # transformed_bones = np.einsum('bmjk,bi->bmi', rotation_matrices, rest_bones_t) + bone_transforms[:, :, 3, :]
+    num_poses = bone_transforms.shape[1]
+    num_bones = rest_bones_t.shape[0]
+    # 初始化结果张量
+    result = np.zeros((num_poses, num_bones, 3))
+
+    # 对每个姿势和每个骨骼进行矩阵乘法
+    for pose in range(num_poses):
+        for bone in range(num_bones):
+            # 取出当前姿势和骨骼的旋转矩阵
+            rotation_matrix = bone_transforms[bone, pose,:3]
+            # 对 rest_bones_t 进行矩阵乘法
+            result[pose, bone] = np.dot(rotation_matrix, rest_bones_t[bone]) + bone_transforms[bone,pose,3]
+
     
-    return transformed_bones  # 形状为 |num_bones| x |num_poses| x 3
+    return result  # 形状为 |num_bones| x |num_poses| x 3
 
 
 def apply_bone_transforms(rest_pose, rest_bone, W, bone_trans):
@@ -151,11 +246,12 @@ def apply_bone_transforms(rest_pose, rest_bone, W, bone_trans):
 
 
 def visualize_SSDR_result_pc():
-    results = np.load("/home/wjx/research/code/DG-Mesh/outputs/ssdr/ssdr_result.npy",allow_pickle=True).item()
+    results = np.load("/home/wjx/research/code/DG-Mesh/outputs/ssdr/jumping/ssdr_result.npy",allow_pickle=True).item()
     rest_pose = results["rest_pose"]
     W = results["W"]
     rest_bone = results["rest_bone"]
     bone_trans = results["Bone_Trans"]
+
     print("W shape:", W.shape)
     print("Bone Transforms shape:", bone_trans.shape)
     print("Rest Bones shape:", rest_bone.shape)
@@ -174,9 +270,10 @@ def visualize_SSDR_result_pc():
     # 计算变换后的点云
     # transformed_points,transformed_bones = apply_bone_transforms(rest_pose, rest_bone, W, bone_trans)
     transformed_points = reconstruct(rest_pose,bone_trans,rest_bone,W)
-    transformed_bones = get_transformed_bones(bone_trans,rest_bone).transpose(1,0,2)
+    transformed_bones = bone_trans[:,:,3,:].transpose(1,0,2)
+    # transformed_bones = get_transformed_bones(bone_trans,rest_bone).transpose(1,0,2)
 
-    print(transformed_points.shape,transformed_bones.shape)
+    # print(transformed_points.shape,transformed_bones.shape)
 
     # 配置绘制参数
     colors = [(1, 0, 0), (0, 0, 1)]  # 每个点云的颜色 (红色和绿色)
@@ -189,7 +286,8 @@ def visualize_SSDR_result_pc():
         'focalpoint': (0, 0, 0)
     }
     image_size = (1024, 768)  # 渲染图像大小
-    output_path = "outputs/ssdr/results_pc"
+    output_path = "outputs/ssdr/jumping/results_pc"
+    check_path(output_path)
 
     for i in range(transformed_points.shape[0]):
         frame_points = transformed_points[i]
@@ -247,4 +345,69 @@ def multi_pc_exp():
 
     visualize_multiple_point_clouds(input_paths, output_path, colors, opacity, sizes, camera_params, image_size)
 
+def visualize_B():
+    results = np.load("/home/wjx/research/code/DG-Mesh/outputs/ssdr/ssdr_result.npy",allow_pickle=True).item()
+    rest_pose = results["rest_pose"]
+    W = results["W"]
+    rest_bone = results["rest_bone"]
+    bone_trans = results["Bone_Trans"]
+    print("W shape:", W.shape)
+    print("Bone Transforms shape:", bone_trans.shape)
+    print("Rest Bones shape:", rest_bone.shape)
+    print("Rest Pose shape:", rest_pose.shape)
+
+    # for i in range(bone_trans.shape[0]):
+    #     bone_index = i  # 选择一个骨骼的索引
+    #     weights = W[:, bone_index]  # 获取对应骨骼的weights
+
+    #     mlab.figure(bgcolor=(1, 1, 1))
+    #     points = rest_pose
+    #     # 通过颜色显示skin weights
+    #     mlab.points3d(points[:, 0], points[:, 1], points[:, 2], weights, scale_factor=0.01, colormap='viridis')
+    #     mlab.title(f"Skinning Weights for Bone {bone_index}")
+    #     mlab.show()
+    # 计算变换后的点云
+    # transformed_points,transformed_bones = apply_bone_transforms(rest_pose, rest_bone, W, bone_trans)
+    # transformed_points = reconstruct(rest_pose,bone_trans,rest_bone,W)
+    transformed_bones = get_transformed_bones(bone_trans,rest_bone).transpose(1,0,2)
+
+    # print(transformed_points.shape,transformed_bones.shape)
+
+    # 配置绘制参数
+    colors = [(1, 0, 0), (0, 0, 1)]  # 每个点云的颜色 (红色和绿色)
+    opacity = [0.5,1]
+    sizes = [0.01, 0.1]  # 每个点云的大小
+    camera_params = {
+        'azimuth': -90,
+        'elevation': 75,
+        'distance': 6,
+        'focalpoint': (0, 0, 0)
+    }
+    image_size = (1024, 768)  # 渲染图像大小
+    output_path = "outputs/ssdr/results_B"
+    check_path(output_path)
+
+    for i in range(transformed_bones.shape[0]):
+        frame_bones = transformed_bones[i]
+        mlab.figure(bgcolor=(1, 1, 1), size=image_size)
+        mlab.clf()
+        
+
+        mlab.options.offscreen = True
+        # mlab.points3d(frame_points[:, 0], frame_points[:, 1], frame_points[:, 2], color=colors[0], scale_factor=sizes[0],opacity=opacity[0])
+        frame_bones = np.array(frame_bones)
+        # print(frame_bones.shape)
+        mlab.points3d(frame_bones[:, 0], frame_bones[:, 1], frame_bones[:, 2], color=colors[1], scale_factor=sizes[1],opacity=opacity[1])
+        mlab.view(azimuth=camera_params.get('azimuth', 90),
+                  elevation=camera_params.get('elevation', 90),
+                  distance=camera_params.get('distance', 10),
+                  focalpoint=camera_params.get('focalpoint', (0, 0, 0)))
+        
+        mlab.savefig(output_path + f"/frame_{i}.png")
+        print(f"image{i} has been saved in {output_path}")
+        mlab.close()
+    
+
 visualize_SSDR_result_pc()
+# visualize_B()
+images_to_gif(path="/home/wjx/research/code/DG-Mesh/outputs/ssdr/jumping/results_pc",fps=10,output_name="jumping/output_trans.gif")
